94b40222e15aaa7d0b583699e5dc457b4f5afecf
LuckyCapybara <215329213+LuckyCapybara@users.noreply.github.com>
2026-07-23 01:46:33 -0600

    WIP: Temporal 'fix' to getName

--- home/names2.asm
+++ home/names2.asm
@@ -20,14 +20,16 @@ GetName::
 
 	; TM names are separate from item names.
 	; BUG: This applies to all names instead of just items.
-	ASSERT NUM_POKEMON_INDEXES < HM01, \
-		"A bug in GetName will get TM/HM names for Pokémon above ${x:HM01}."
+	; TODO: Fix this bug. Temporarily disabled.
+	; ASSERT NUM_POKEMON_INDEXES < HM01, \
+	; 	"A bug in GetName will get TM/HM names for Pokémon above ${x:HM01}."
 	ASSERT NUM_ATTACKS < HM01, \
 		"A bug in GetName will get TM/HM names for moves above ${x:HM01}."
 	ASSERT NUM_TRAINERS < HM01, \
 		"A bug in GetName will get TM/HM names for trainers above ${x:HM01}."
-	cp HM01
-	jp nc, GetMachineName
+	; TODO: Fix this bug. Temporarily disabled.
+	; cp HM01
+	; jp nc, GetMachineName
 
 	ldh a, [hLoadedROMBank]
 	push af
